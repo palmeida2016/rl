df074978f120bf36ed17fc726fd3522457f20f08
Pedro Almeida <palmeida2016@fau.edu>
2022-01-05 23:39:07 -0500

    Piece movement and tracking added

--- examples/nav.py
+++ examples/nav.py
@@ -1,13 +1,26 @@
 import numpy as np
+import time
 
 class Piece:
-    def __init__(self, name, x, y):
+    def __init__(self, name, x, y, size):
         '''
         Initialize Piece with its position
         '''
         self.name = name
         self.x = x
         self.y = y
+        self.size = size
+
+        self.key = {
+        0: [1, 0],
+        1: [-1, 0],
+        2: [0, 1],
+        3: [0, -1],
+        4: [1, 1],
+        5: [1, -1],
+        6: [-1, 1],
+        7: [-1, -1]
+        }
 
     def __repr__(self):
         '''
@@ -37,13 +50,26 @@ class Piece:
         '''
         # Check if objects both are pieces
         assert(type(otherPiece) == type(self))
-        
+       
+       # Check if objects are in the same place
         return (self.x == otherPiece.x and self.y == otherPiece.y)
+        
 
-    def move(self, deltaX, deltaY):
+    def move(self, action):
         '''
         Update position of piece with delta x values
         '''
+        # Convert from action to delta values
+        (deltaX, deltaY) = self.key.get(action)
+        
+        # Check if move is possible
+        if self.x+deltaX < 0 or self.x+deltaX > self.size-1:
+            deltaX = 0
+
+        if self.y+deltaY < 0 or self.y+deltaY > self.size-1:
+            deltaY = 0
+        
+        # Update positions of piece with movement
         self.x += deltaX
         self.y += deltaY
 
@@ -53,11 +79,12 @@ class Piece:
         '''
         return (self.x,self.y)
 
+
 class Env:
-    def __init__(self, size = 10):
-        # Define Layout
+    def __init__(self, size = 8):
+        # Define grid size
         self.size = size
-      
+
         # Define Empty List of Pieces
         self.pieces = []
   
@@ -82,20 +109,27 @@ class Env:
                         y = np.random.randint(self.size)
                 
                 # If position is accepted, add to pieces
-                self.pieces.append(Piece(name,x,y))
+                self.pieces.append(Piece(name,x,y,self.size))
 
         # Add all objects to the list
-        populate(self, nPolice, 'police')
         populate(self, nThief, 'thief')
         populate(self, nGold, 'gold')
-
+        populate(self, nPolice, 'police')
+    
+    def getDistanceThief(self):
+        # Get thief object in list
+        thief = next(piece for piece in self.pieces if piece.name == 'thief')
+        
+        # Return distance to each object
+        return [thief-piece for piece in self.pieces if piece.name != 'thief']
+        
 
     def display(self):
         '''
         Display current configuration of the grid
         '''
         for x in range(self.size):
-            print('--------------------')
+            print((2*self.size+1)*'-')
             for y in range(self.size):
                 print('|',end='')
                 if (x,y) in [piece.getPos() for piece in self.pieces]: # If there is piece
@@ -105,11 +139,13 @@ class Env:
                     # Print object at that position with key
                     print(self.key.get(p.name), end='')
                 else:
-                    print(self.key.get('empty'),end='')
-            print('')
-        print('--------------------')
+                    # If no object print empty space
+                    print(self.key.get('empty'), end='')
+            print('|')
+        print((2*self.size+1)*'-')
 
 if __name__ == '__main__':
     env = Env()
     env.initializePositions()
     env.display()
+    env.getDistanceThief()
